96ec1583df63e85d6816df9637e53b707486898f
Terry <terrybmochire@gmail.com>
2022-08-24 18:59:42 +0300

    Add header file.

--- binary_trees.h
+++ binary_trees.h
@@ -2,9 +2,6 @@
 #define _BINARY_TREES_H_
 
 #include <stddef.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -14,17 +11,14 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
+void binary_tree_print(const binary_tree_t *);
 
-#endif
+#endif /* _BINARY_TREES_H_ */
