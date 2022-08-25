73b4443587cfa2f2b20db444df969f012b77b555
MercyMukiri <mwmukiri@gmail.com>
2022-08-25 11:43:27 -0700

    Edit betty errors and add file 11-binary_tree_size.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * size_t binary_tree_depth(const binary_tree_t *tree)
+ * binary_tree_depth - measures the depth of a node in a binary tree
  *
  * @tree: a pointer to the node to measure the depth
  *
